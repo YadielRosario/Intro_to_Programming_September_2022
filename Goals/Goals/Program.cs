2c6c8a0fda3671eef4b1db6edb8c86768cb470a7
Yadiel Rosario Cruz <yadiel_rosario_cruz@progressive.com>
2022-09-12 16:50:38 -0400

    Initial code entry

--- Goals/Goals/Program.cs
+++ Goals/Goals/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your goal for today:");
+string? newGoal = Console.ReadLine();
+
+bool saveGoal = false;
+if (newGoal != null)
+{
+    Console.WriteLine($"For {DateTime.Today} your goal is: \n {newGoal} \n Save Changes? (Y/N)");
+    string? saveString = Console.ReadLine();
+    if (saveString == null)
+    {
+        Console.WriteLine("Please enter a response:");
+        return;
+    }
+    else if (saveString.ToLower() == "y" || saveString.ToLower() == "yes")
+    {
+        saveGoal = true;
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a goal:");
+}
